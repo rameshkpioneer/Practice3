eb6c096c8a1d2342c832a027f731724fdcfc0a81
rameshkolanuvada <rameshkpioneer@gmail.com>
2017-11-29 15:51:24 +0530

    Added some Code for new changes

--- MyPractice/src/ram/practice/collectio/MyArrayList.java
+++ MyPractice/src/ram/practice/collectio/MyArrayList.java
@@ -5,51 +5,99 @@ import java.util.Arrays;
 public class MyArrayList {
 
 	
-	Object  arr[] ;
-	int capacity = 3;
-	int count = 0;
+	Object  myStore[] ;
+	int capacity =10;
+	int actSize = 0;
+	
 	public MyArrayList(){
-		arr = new Object[capacity];
+		
+		myStore = new Object[capacity];
+		
 	}
 	
 	public MyArrayList(int cap){
+		
 		this.capacity = cap;
-		arr = new Object[capacity];
+		myStore = new Object[capacity];
+	
 	}
 	
 	
 	public void add(Object o){
-		if ( count < arr.length){
-			arr[count] = o;
-		}else{
-		 arr = Arrays.copyOf(arr, capacity*2);
+		
+		if ( myStore.length - actSize <=5 ){
+			 myStore = Arrays.copyOf(myStore, capacity*2);
 		}
-		count ++;
+		
+		myStore[actSize ++] = o;
 	}
 	
-	public void  get(){
+	public Object  get(int index){
+		return myStore[index];
+	}
+	
+	
+	public Object remove(int index){
 		
+	
+	
+		if(index < actSize){
+			
+	            Object obj = myStore[index];
+	            myStore[index] = null;
+	            int tmp = index;
+	            
+	            while(tmp < actSize-1){
+	                myStore[tmp] = myStore[tmp+1];
+	                myStore[tmp+1] = null;
+	                tmp++;
+	            }
+	            
+	            actSize--;
+	            return obj;
+	            
+	        } else {
+	        	
+	            throw new ArrayIndexOutOfBoundsException();
+	            
+	        }
+	}
+	
+	public int size(){
+		return actSize;
 	}
 	
 	public String toString(){
-		
-		return "TEST";
+		return Arrays.toString(myStore);
 	}
-	public void remove(){
-		
-	}
+	public void display() {
+        for (int i = 0; i < actSize; i++) {
+               System.out.print(myStore[i] + " ");
+        }
+ }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
 		MyArrayList ma = new MyArrayList();
-		ma.add("RAmesh");
-		ma.add("Rajesh");
-		ma.add("Rajeev");
-		System.out.println("  "+ ma);
+		ma.add(0);
+		ma.add(1);
+		ma.add(2);
+		ma.add(2);
+		ma.add(2);
+		ma.add(2);
+		ma.add(2);
 		
+		System.out.println(" Length of  "+ ma.size());
+	
+	//	ma.remove(1);
+	//	System.out.println(" Elements in the array  "+ ma.toString());
 		
+		System.out.println(" Length after removing an element  "+ ma.size());
+		System.out.println(" Indexed Element in the array  "+ ma.get(0));
 		
+		ma.display(); 
+
 	}
 
 }
